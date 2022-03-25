49901879f914c6e3fef4b212e3406ec8508f690d
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-03-25 20:09:28 -0300

    classes

---
+++ BankOOP/Bank.java
@@ -0,0 +1,11 @@
+public class Bank {
+  private String name;
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
---
+++ BankOOP/Client.java
@@ -0,0 +1,20 @@
+public class Client {
+  private int agencia;
+  private int number;
+
+  public int getAgencia() {
+    return this.agencia;
+  }
+
+  public void setAgencia(int agencia) {
+    this.agencia = agencia;
+  }
+
+  public int getNumber() {
+    return this.number;
+  }
+
+  public void setNumber(int number) {
+    this.number = number;
+  }
+}
